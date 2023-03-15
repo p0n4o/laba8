79085a68c8daa7f328629417e9d5b55d3d9ec3ae
p0n40 <nazar_2004@icloud.com>
2023-03-15 10:27:24 +0300

    file changes 5

--- main.c
+++ main.c
@@ -10,7 +10,8 @@ void search(int* n, EMPLOYEE **emp){ // Функция поиска набора
     int max_height = 0;
     int min_price = 100000;
     int p = 0;
-    int res;
+    int l = 0;
+    int res[*n];
 
     for(int i = 0; i<*n; i++){ // Находим минимальное значение цены среди всех наборов
         if(emp[i]->price < min_price){
@@ -18,26 +19,44 @@ void search(int* n, EMPLOYEE **emp){ // Функция поиска набора
         }
     }
 
-    for(int j = 0; j<*n; j++) { // В заранее объявленный массив записываем номера всех наборов с минимальной ценой
+
+    for(int j = 0; j<*n; j++) { // В заранее объявленный массив записываем индексы всех наборов с минимальной ценой
         if (emp[j]->price == min_price){
             new[p] = j;
             p++;
         }
     }
 
-    for (int k = 0; k < p; k++) { // Среди всех наборов вошедших в массив "new" находим набор с наибольшей высотой
-        if (emp[new[k]]->ftr.height > max_height) {
-            max_height = emp[new[k]]->ftr.height;
-            res = new[k];
+    for(int m = 0; m < p; m++){ // Находим максимальное значение высоты среди всех наборов минимальной цены
+        if(emp[new[m]]->ftr.height > max_height){
+            max_height = emp[new[m]]->ftr.height;
         }
     }
 
-    // Вывод набора, удовлетворяющего условию
-    printf(ANSI_COLOR_WHITE "Название самого высокого набора минимальной цены:" ANSI_COLOR_GREEN "\t\"%s\"\n", emp[res]->name);
-    printf(ANSI_COLOR_YELLOW "Цена:     %li\n", emp[res]->price);
-    printf(ANSI_COLOR_WHITE "Вес:      %li\n", emp[res]->ftr.weight);
-    printf(ANSI_COLOR_YELLOW "Высота:   %li\n", emp[res]->ftr.height);
-    printf(ANSI_COLOR_WHITE "Ширина:   %li\n\n", emp[res]->ftr.width);
+    for (int k = 0; k < p; k++) { // Среди всех наборов, индексы которых вошли в массив "new", находим наборы с наибольшей высотой
+        if (emp[new[k]]->ftr.height == max_height) {
+            res[l] = new[k];
+            l++;
+        }
+    }
+    if (l > 1){
+        for (int s = 0; s < l; s++) {
+            // Если наборов, удовлетворяющих условию, несколько, то выводим с учетом нуменклатуры
+            printf(ANSI_COLOR_WHITE "Название %d-го самого высокого набора минимальной цены:" ANSI_COLOR_GREEN "\t\"%s\"\n", s+1, emp[res[s]]->name);
+            printf(ANSI_COLOR_YELLOW "Цена:     %li\n", emp[res[s]]->price);
+            printf(ANSI_COLOR_WHITE "Вес:      %li\n", emp[res[s]]->ftr.weight);
+            printf(ANSI_COLOR_YELLOW "Высота:   %li\n", emp[res[s]]->ftr.height);
+            printf(ANSI_COLOR_WHITE "Ширина:   %li\n\n", emp[res[s]]->ftr.width);
+        }
+    }
+    else{
+        // Если один набор удовлетворяет условию, то просто выводим
+        printf(ANSI_COLOR_WHITE "Название самого высокого набора минимальной цены:" ANSI_COLOR_GREEN "\t\"%s\"\n", emp[res[0]]->name);
+        printf(ANSI_COLOR_YELLOW "Цена:     %li\n", emp[res[0]]->price);
+        printf(ANSI_COLOR_WHITE "Вес:      %li\n", emp[res[0]]->ftr.weight);
+        printf(ANSI_COLOR_YELLOW "Высота:   %li\n", emp[res[0]]->ftr.height);
+        printf(ANSI_COLOR_WHITE "Ширина:   %li\n\n", emp[res[0]]->ftr.width);
+    }
 }
 
 
